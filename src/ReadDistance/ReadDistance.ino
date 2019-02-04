a635187da9728d686675b754c3b47e59448680bc
Unknown <jruben4@yahoo.com>
2019-02-04 17:15:34 -0600

    Progress...

--- src/ReadDistance/ReadDistance.ino
+++ src/ReadDistance/ReadDistance.ino
@@ -7,26 +7,63 @@
 #include "SparkFun_VL53L1X.h"
 
 //Optional interrupt and shutdown pins.
-#define SHUTDOWN_PIN 2
-#define INTERRUPT_PIN 3
+#define SHUTDOWN_PIN_ONE 8
+#define INTERRUPT_PIN_ONE 9
+#define SHUTDOWN_PIN_TWO 10
+#define INTERRUPT_PIN_TWO 11
 
-SFEVL53L1X distanceSensor1;
-SFEVL53L1X distanceSensor2;
+#define SENSOR_ADDRESS_ONE 20
+//SFEVL53L1X distanceSensor1;
+SFEVL53L1X distanceSensor1(Wire, SHUTDOWN_PIN_ONE, INTERRUPT_PIN_ONE);
+SFEVL53L1X distanceSensor2(Wire, SHUTDOWN_PIN_TWO, INTERRUPT_PIN_TWO);
 
-//Uncomment the following line to use the optional shutdown and interrupt pins.
-//SFEVL53L1X distanceSensor(Wire, SHUTDOWN_PIN, INTERRUPT_PIN);
 
 void setup(void)
 {
   Wire.begin();
 
   Serial.begin(9600);
- // Serial.println("VL53L1X Qwiic Test");
 
+  Serial.println("Booting...");
+  
+  distanceSensor1.sensorOn();
+   
   if (distanceSensor1.begin() == false)
   {
-   // Serial.println("Sensor online!");
+    Serial.println("Sensor 1 online!");
   }
+  Serial.print("Sensor1 Address: ");
+  Serial.println(distanceSensor1.getI2CAddress());
+
+  distanceSensor1.sensorOff();
+
+
+  distanceSensor2.sensorOn();
+   
+  if (distanceSensor2.begin() == false)
+  {
+    Serial.println("Sensor 2 online!");
+  }
+  Serial.print("Sensor2 Address: ");
+  Serial.println(distanceSensor2.getI2CAddress());
+
+  distanceSensor2.sensorOff();
+
+  
+/*
+  if (distanceSensor2.begin() == false)
+  {
+    Serial.println("Sensor 2 online!");
+  }
+  Serial.println(distanceSensor2.getI2CAddress());
+*/  
+
+//  distanceSensor2.sensorOff();
+//  distanceSensor1.setI2CAddress(SENSOR_ADDRESS_ONE);
+//  distanceSensor2.sensorOn();
+// Serial.println("Sensor 1 Address: " + distanceSensor1.getSensorID());
+//  Serial.println("Sensor 2 Address: " + distanceSensor2.getI2CAddress());
+ 
 }
 
 void loop(void)
@@ -36,9 +73,9 @@ void loop(void)
   distanceSensor1.stopRanging();
 
 
-  Serial.print("S1");
-  Serial.print(distance);
-  Serial.print("E");
+  //Serial.print("S1");
+  //Serial.println(distance);
+  //Serial.print("E");
 
 }
 
