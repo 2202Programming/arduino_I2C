eb69e6aed50aab0a52c512a9e4d23f912b176fda
Unknown <jruben4@yahoo.com>
2019-02-04 19:14:09 -0600

    2 sensors working

--- src/ReadDistance/ReadDistance.ino
+++ src/ReadDistance/ReadDistance.ino
@@ -12,8 +12,7 @@
 #define SHUTDOWN_PIN_TWO 10
 #define INTERRUPT_PIN_TWO 11
 
-#define SENSOR_ADDRESS_ONE 20
-//SFEVL53L1X distanceSensor1;
+
 SFEVL53L1X distanceSensor1(Wire, SHUTDOWN_PIN_ONE, INTERRUPT_PIN_ONE);
 SFEVL53L1X distanceSensor2(Wire, SHUTDOWN_PIN_TWO, INTERRUPT_PIN_TWO);
 
@@ -24,58 +23,27 @@ void setup(void)
 
   Serial.begin(9600);
 
-  Serial.println("Booting...");
-  
-  distanceSensor1.sensorOn();
-   
-  if (distanceSensor1.begin() == false)
-  {
-    Serial.println("Sensor 1 online!");
-  }
-  Serial.print("Sensor1 Address: ");
-  Serial.println(distanceSensor1.getI2CAddress());
-
-  distanceSensor1.sensorOff();
-
-
-  distanceSensor2.sensorOn();
-   
-  if (distanceSensor2.begin() == false)
-  {
-    Serial.println("Sensor 2 online!");
-  }
-  Serial.print("Sensor2 Address: ");
-  Serial.println(distanceSensor2.getI2CAddress());
-
-  distanceSensor2.sensorOff();
-
-  
-/*
-  if (distanceSensor2.begin() == false)
-  {
-    Serial.println("Sensor 2 online!");
-  }
-  Serial.println(distanceSensor2.getI2CAddress());
-*/  
-
-//  distanceSensor2.sensorOff();
-//  distanceSensor1.setI2CAddress(SENSOR_ADDRESS_ONE);
-//  distanceSensor2.sensorOn();
-// Serial.println("Sensor 1 Address: " + distanceSensor1.getSensorID());
-//  Serial.println("Sensor 2 Address: " + distanceSensor2.getI2CAddress());
- 
 }
 
 void loop(void)
 {
-  distanceSensor1.startRanging(); //Write configuration bytes to initiate measurement
-  int distance = distanceSensor1.getDistance(); //Get the result of the measurement from the sensor
-  distanceSensor1.stopRanging();
+  distanceSensor1.sensorOn();
+  distanceSensor1.init();
+  int distance1 = distanceSensor1.getDistance(); //Get the result of the measurement from the sensor
+  distanceSensor1.sensorOff();
+  Serial.print("S1");
+  Serial.print(distance1);
+  Serial.print("E");
 
 
-  //Serial.print("S1");
-  //Serial.println(distance);
-  //Serial.print("E");
+  distanceSensor2.sensorOn();
+  distanceSensor2.init();
+  int distance2 = distanceSensor2.getDistance(); //Get the result of the measurement from the sensor
+  distanceSensor2.sensorOff();
+
+  Serial.print("S2");
+  Serial.print(distance2);
+  Serial.print("E");
 
 }
 
