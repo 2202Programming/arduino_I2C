2f3bfd31324a53e49f1ca040aadea5ccd301fe08
Unknown <jruben4@yahoo.com>
2019-02-04 20:49:36 -0600

    Simultaneous sensor readings, no stop ranging (crosstalk?)

--- src/ReadDistance/ReadDistance.ino
+++ src/ReadDistance/ReadDistance.ino
@@ -11,7 +11,8 @@
 #define INTERRUPT_PIN_ONE 9
 #define SHUTDOWN_PIN_TWO 10
 #define INTERRUPT_PIN_TWO 11
-
+#define TOF_ADDRESS_ONE 82
+#define TOF_ADDRESS_TWO 83
 
 SFEVL53L1X distanceSensor1(Wire, SHUTDOWN_PIN_ONE, INTERRUPT_PIN_ONE);
 SFEVL53L1X distanceSensor2(Wire, SHUTDOWN_PIN_TWO, INTERRUPT_PIN_TWO);
@@ -22,24 +23,31 @@ void setup(void)
   Wire.begin();
 
   Serial.begin(115200);
+  //Turn off Sensor 1 so we can reprogram address on #2 
+  distanceSensor1.sensorOff();
+  distanceSensor2.sensorOn();
+  distanceSensor2.init();
+  distanceSensor2.setI2CAddress(TOF_ADDRESS_TWO);
+ 
 
+  distanceSensor1.sensorOn();
+  distanceSensor1.init();
+  
 }
 
 void loop(void)
 {
-  distanceSensor1.sensorOn();
-  distanceSensor1.init();
+
   int distance1 = distanceSensor1.getDistance(); //Get the result of the measurement from the sensor
-  distanceSensor1.sensorOff();
+
   Serial.print("S1");
   Serial.print(distance1);
   Serial.print("E");
 
 
-  distanceSensor2.sensorOn();
-  distanceSensor2.init();
+
   int distance2 = distanceSensor2.getDistance(); //Get the result of the measurement from the sensor
-  distanceSensor2.sensorOff();
+
 
   Serial.print("S2");
   Serial.print(distance2);
